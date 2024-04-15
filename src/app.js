f6460727b3862f6fd09e5beaa0d53ea697b081d8
Roronoa Zoro <shivamsharma8434202092@gmail.com>
2024-04-16 00:46:25 +0530

    static public changes

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ app.use(cors({
 
 app.use(express.json({ limit: "20Kb" }));
 app.use(express.urlencoded({ extended: true, limit: "20Kb" }))
-app.use(express.static("/public"))  // any folder that can be accessed for storing file in the folder
+app.use(express.static("public"))  // any folder that can be accessed for storing file in the folder
 
 app.use(cookieParser())
 

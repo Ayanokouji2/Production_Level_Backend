fd5aab604375b4be66d346365b833cf04329f506
Roronoa Zoro <shivamsharma8434202092@gmail.com>
2024-05-03 22:02:20 +0530

    Refactor user and video controllers for improved error handling and code readability

--- src/Controllers/user.controller.js
+++ src/Controllers/user.controller.js
@@ -32,137 +32,129 @@ const cookieOption = {
 
 
 const userRegister = asyncHandler(async (req, res) => {
-    // try {
-        /*
-         * [✔️] get user details from frontend
-         * [✔️] validate the fields 
-         * [✔️] check if user exists: username, email
-         * [✔️] check for images : avatar, coverImage -> 
-         * [✔️] upload it to cloudinary
-         * [✔️] create the user Object and then save it to db
-         * [✔️] check if response is comming or not
-         * [✔️] remove the hashedpassword -> password and remove refreshtoken
-         * [✔️] send the response to the frontend
-         */
+    /*
+     * [✔️] get user details from frontend
+     * [✔️] validate the fields 
+     * [✔️] check if user exists: username, email
+     * [✔️] check for images : avatar, coverImage -> 
+     * [✔️] upload it to cloudinary
+     * [✔️] create the user Object and then save it to db
+     * [✔️] check if response is comming or not
+     * [✔️] remove the hashedpassword -> password and remove refreshtoken
+     * [✔️] send the response to the frontend
+     */
 
-        const { username, email, password, fullName } = req.body
+    const { username, email, password, fullName } = req.body
 
-        if ([username, email, password, fullName].some((field) => field === undefined || field?.trim() === "")) {
-            // console.log("command was here")
-            throw new ApiError(400, " Required Missing Credentials thrown by the command")
-        }
-        const existing_user = await User.findOne({ $or: [{ username }, { email }] })
+    if ([username, email, password, fullName].some((field) => field === undefined || field?.trim() === "")) {
+        throw new ApiError(400, " Required Missing Credentials thrown by the command")
+    }
+    const existing_user = await User.findOne({ $or: [{ username }, { email }] })
 
-        if (existing_user) {
-            return res.status(409).json(new ApiError(409, "Existing User"))
-        }
+    if (existing_user) {
+        throw new ApiError(409, "Existing User")
+    }
 
-        const avatarLocalPath = req.files?.avatar[0]?.path      // If this throws error like cannot read property of undefined even when the file is uploaded then it is because of the multer sends an connect.sid and if this is not updated then it will not send the file to the req.files. So, to solve this issue we need to create a new request in postman and then try again.
+    const avatarLocalPath = req.files?.avatar[0]?.path      // If this throws error like cannot read property of undefined even when the file is uploaded then it is because of the multer sends an connect.sid and if this is not updated then it will not send the file to the req.files. So, to solve this issue we need to create a new request in postman and then try again.
 
-        var coverImageLocalPath = ""
-        if (req.files.coverImage)
-            coverImageLocalPath = req.files?.coverImage[0]?.path
+    var coverImageLocalPath = ""
+    if (req.files.coverImage)
+        coverImageLocalPath = req.files?.coverImage[0]?.path
 
-        if (!avatarLocalPath) {
-            return res.status(400).json(new ApiError(400, 'Avatar is required... Must Upload'))
-        }
+    if (!avatarLocalPath) {
+        throw new ApiError(400, 'Avatar is required... Must Upload')
+    }
 
-        const avatar = await uploadOnCloudinary(avatarLocalPath);
+    const avatar = await uploadOnCloudinary(avatarLocalPath);
 
-        if (!avatar) {
-            return res.status(500).json(new ApiError(500, 'Avatar Upload failed..'))
-        }
+    if (!avatar) {
+        throw new ApiError(500, 'Avatar Upload failed..')
+    }
 
-        var coverImage
-        if (coverImageLocalPath && coverImageLocalPath !== "") {
-            coverImage = await uploadOnCloudinary(coverImageLocalPath)
-        }
+    var coverImage
+    if (coverImageLocalPath && coverImageLocalPath !== "") {
+        coverImage = await uploadOnCloudinary(coverImageLocalPath)
+    }
 
-        if (coverImageLocalPath !== "" && !coverImage) {
-            return res.status(500).json(new ApiError(500, 'cover Image  Upload failed..'))
-        }
+    if (coverImageLocalPath !== "" && !coverImage) {
+        throw new ApiError(500, 'cover Image  Upload failed..')
+    }
 
-        const user_to_save = {
-            username,
-            email,
-            fullName,
-            password,
-            avatar: {
-                url: avatar.url,
-                public_id: avatar.public_id
-            },
-            coverImage: {
-                url: coverImage?.url,
-                public_id: coverImage?.public_id
-            },
-        }
+    const user_to_save = {
+        username,
+        email,
+        fullName,
+        password,
+        avatar: {
+            url: avatar.url,
+            public_id: avatar.public_id
+        },
+        coverImage: {
+            url: coverImage?.url,
+            public_id: coverImage?.public_id
+        },
+    }
 
-        const user = await User.create(user_to_save);
+    const user = await User.create(user_to_save);
 
-        const created_User = await User.findById(user._id).select("-password -refreshToken")
+    const created_User = await User.findById(user._id).select("-password -refreshToken")
 
-        if (!created_User) {
-            return res.status(409).json(new ApiError(500, "Creating User failed ...!"))
-        }
+    if (!created_User) {
+        throw new ApiError(500, "Creating User failed ...!")    
+    }
 
-        return res.status(201).json(new ApiResponse(200, created_User, "User Created Succesfully"))
+    return res.status(201).json(new ApiResponse(200, created_User, "User Created Succesfully"))
 
-    // } catch (error) {
-    //     return res.status(500).json(new ApiError(500, "Registeration Failed "+ error.message))
-    // }
 })
 
 
 const userLogin = asyncHandler(async (req, res) => {
-    try {
-        /**
-         *  [✔️] Get (username or email) and password from req.body
-         *  [✔️] Validate username and password
-         *  [✔️] fetch user using username or email
-         *  [✔️] Check if user exists 
-         *  [✔️] validate the password
-         *  [✔️] generate ACCESS & REFRESH jwt token
-         *  [✔️] send secure cookies 
-         *  [✔️] return the response
-         */
-        const { username, password, email } = req.body
+    /*
+     *  [✔️] Get (username or email) and password from req.body
+     *  [✔️] Validate username and password
+     *  [✔️] fetch user using username or email
+     *  [✔️] Check if user exists 
+     *  [✔️] validate the password
+     *  [✔️] generate ACCESS & REFRESH jwt token
+     *  [✔️] send secure cookies 
+     *  [✔️] return the response
+     */
+    const { username, password, email } = req.body
 
-        if ((!username && !email) || !password) {
-            return res.status(400).json(new ApiError(400, "Fields are Required"))
-        }
-
-        const user_from_db = await User.findOne({ $or: [{ username: username.toLowerCase() }, { email }] })
-
-        if (!user_from_db) {
-            return res.status(404).json(new ApiError(404, "No existing user"))
-        }
-
-        const isPasswordCorrect = await user_from_db.isPasswordCorrect(password)  // the methods are not the part of the Database Model i.e (User) but it is the methods of the instance i.e (user_from_db)
-        if (!isPasswordCorrect) {
-            return res.status(401).json(new ApiError(404, "Invalid credentials"))
-        }
-
-        const { refreshToken, accessToken } = await generateRefreshAndAccessToken(user_from_db._id);
-
-        //! Why i feteched the user again ?
-        //? Because in the generateRefreshAndAccessToken() we updated the field accessToken, bt the field is not accessible by the user_from_db 
-
-        const user_updated = await User.findOne({ $or: [{ username }, { email }] })
-            .select("-password")
-
-
-        // To secure the cookie by making it accessible from server not from frontend
-
-
-        res
-            .status(200)
-            .cookie("refreshToken", refreshToken, cookieOption)
-            .cookie("accessToken", accessToken, cookieOption)
-            .json(new ApiResponse(200, user_updated, " User authenticated ready for Login"))
-
-    } catch (error) {
-        res.status(500).json(new ApiError(500, "Error While Logging Out...!"))
+    if ((!username && !email) || !password) {
+        throw new ApiError(400, "Fields are Required")
     }
+
+    const user_from_db = await User.findOne({ $or: [{ username: username.toLowerCase() }, { email }] })
+
+    if (!user_from_db) {
+        throw new ApiError(404, "No existing user")
+    }
+
+    const isPasswordCorrect = await user_from_db.isPasswordCorrect(password)  // the methods are not the part of the Database Model i.e (User) but it is the methods of the instance i.e (user_from_db)
+    if (!isPasswordCorrect) {
+        throw new ApiError(401, "Invalid credentials")
+    }
+
+    const { refreshToken, accessToken } = await generateRefreshAndAccessToken(user_from_db._id);
+
+    //! Why i feteched the user again ?
+    //? Because in the generateRefreshAndAccessToken() we updated the field accessToken, bt the field is not accessible by the user_from_db 
+
+    const user_updated = await User.findOne({ $or: [{ username: username.toLowerCase() }, { email }] })
+        .select("-password")
+
+
+    // To secure the cookie by making it accessible from server not from frontend
+
+
+    res
+        .status(200)
+        .cookie("refreshToken", refreshToken, cookieOption)
+        .cookie("accessToken", accessToken, cookieOption)
+        .json(new ApiResponse(200, user_updated, " User authenticated ready for Login"))
+
+
 })
 
 

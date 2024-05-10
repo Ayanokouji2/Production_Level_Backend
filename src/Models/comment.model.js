ffb0793a4631ba0046e8061f9ba390adf8f2275a
Roronoa Zoro <shivamsharma8434202092@gmail.com>
2024-05-10 23:38:14 +0530

    Comment Model added

---
+++ src/Models/comment.model.js
@@ -0,0 +1,22 @@
+import { Schema, model } from 'mongoose'
+
+const commentSchema = new Schema(
+    {
+        owner:{
+            type:Schema.Types.ObjectId,
+            ref:"User"
+        },
+        video:{
+            type:Schema.Types.ObjectId,
+            ref:"Video"
+        },
+        content:{
+            type:String,
+            required:true
+        }
+    },{
+        timestamps:true
+    }
+)
+
+export const Comment = model('Comment', commentSchema)

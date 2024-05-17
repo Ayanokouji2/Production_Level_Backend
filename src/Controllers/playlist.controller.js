4704e3e93da978183d68fcbd6e5d97b3b8580571
Roronoa Zoro <shivamsharma8434202092@gmail.com>
2024-05-17 19:42:53 +0530

    remove duplication of video from playlist

--- src/Controllers/playlist.controller.js
+++ src/Controllers/playlist.controller.js
@@ -2,29 +2,34 @@ import { Playlist } from '../Models/playlist.model.js';
 import asyncHandler from '../utils/asyncHandler.js';
 import { ApiResponse } from '../utils/ApiResponse.js';
 import { ApiError } from '../utils/ApiError.js';
+import mongoose from 'mongoose';
 
-export const addVideoToPlaylist = asyncHandler( async ( req, res ) =>{
-    const { playlistId } = req.body
+export const addVideoToPlaylist = asyncHandler(async (req, res) => {
+    var playlistId = req.body.playlistId
 
     const videoId = req.params.videoId
 
-    if(!videoId)
+    if (!videoId)
         throw new ApiError(400, 'Video ID is required')
-    
+
+    if (!playlistId) {
+        playlistId = new mongoose.Types.ObjectId()
+    }
+
     const videos_playlist = await Playlist.findById(playlistId).select("videos")
 
-    if(videos_playlist.videos.includes(videoId))
+    if (videos_playlist && videos_playlist.videos.includes(videoId))
         return res.status(200).json(new ApiResponse(200, videos_playlist, 'Video already in playlist'))
 
+
     const updated_playlist = await Playlist.findByIdAndUpdate(playlistId, {
-        $push:{
+        $push: {
             videos: videoId
         }
-    },{ new: true, useFindAndModify: false })
+    }, { new: true, useFindAndModify: false, upsert: true })
 
-    // console.log(updated_playlist)
-    if(!updated_playlist)
+    if (!updated_playlist)
         throw new ApiError(404, 'Playlist not found')
 
-    res.status(200).json(new ApiResponse(200, updated_playlist,'Video added to playlist')) 
+    res.status(200).json(new ApiResponse(200, updated_playlist, 'Video added to playlist'))
 })
